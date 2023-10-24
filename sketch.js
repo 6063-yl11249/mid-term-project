d158e45ed9eb7012f8e81aadfef219f2a634c8c9
Yuanqing <yl11249@nyu.edu>
2023-10-23 20:17:02 -0400

    initial design

--- sketch.js
+++ sketch.js
@@ -6,6 +6,10 @@ let colorIndex = 0;
 
 let squareMouse = [];
 
+let numLines = 16;
+let startTime;
+let speed = 0.5;
+
 let mFont;
 let mSize = 120;
 let word = "The Metaverse";
@@ -16,13 +20,14 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  startTime = millis();
 }
 
 function draw() {
   background(0);
 
   //Draw the square moving with mouse
-squareMouse.push({ x: mouseX, y: mouseY, alpha: 5 });
+  squareMouse.push({ x: mouseX, y: mouseY, alpha: 5 });
   if (squareMouse.length > 10) {
     squareMouse.shift();
   }
@@ -31,27 +36,44 @@ squareMouse.push({ x: mouseX, y: mouseY, alpha: 5 });
     let currentSquare = squareMouse[i];
     stroke("deeppink");
     rect(currentSquare.x, currentSquare.y, 80, 80);
-    currentSquare.alpha += 10; 
+    currentSquare.alpha += 10;
     fill(255, currentSquare.alpha);
   }
-// Remove completely faded out ellipses
-  squareMouse = squareMouse.filter(rect => rect.alpha > 0);
-
-  //Add the mouse click function to create effect
-
+  // Remove completely faded out squares
+  squareMouse = squareMouse.filter((rect) => rect.alpha > 0);
 
   // Draw the lines
-  let numLines = 12;
-  for (let l = 0; l <= numLines; l += 1) {
-    x0 = map(l, 0, numLines, 0, width);
-    x1 = map(l, 0, numLines, (4 / 10) * width, (6 / 10) * width);
-    x2 = map(l, 0, numLines, -width, 2 * width);
-    noFill();
-    stroke(255);
-    line(x0, 0, x1, height / 2);
-    line(x1, height / 2, x2, height);
+  let currentTime = millis() - startTime;
+
+  for (let i = 0; i <= numLines; i++) {
+    let x0 = map(i, 0, numLines, 0, width); 
+    let t = sin(currentTime * 0.001 * speed + PI / 2); // Adjust the speed factor with a phase shift
+    let x1Initial = map(i, 0, numLines, (4 / 10) * width, (6 / 10) * width);
+    let x2 = map(i, 0, numLines, 0, width);
+    let x1 = x1Initial;
+
+    if (i <= numLines / 2) {
+      // Move to the left for the first half
+      let moveDistance = map(abs(t), 0, 1, 0, (4 / 10) * width);
+      if (t > 0) {
+        x1 = max(x0, x1Initial - moveDistance);
+      } else {
+        x1 = min(x1Initial, x1Initial + moveDistance);
+      }
+    } else {
+      // Move to the right for the second half
+      let moveDistance = map(abs(t), 0, 1, 0, (4 / 10) * width);
+      if (t > 0) {
+        x1 = min(x2, x1Initial + moveDistance);
+      } else {
+        x1 = max(x1Initial, x1Initial - moveDistance);
+      }
+    }
+
+    let y1 = height / 2;
+    line(x0, 0, x1, y1);
+    line(x1, y1, x2, height);
   }
-  //Make one side of lines moving with mouse
 
   // Create the squares fade in/ out
   rectMode(CENTER);
@@ -93,5 +115,5 @@ squareMouse.push({ x: mouseX, y: mouseY, alpha: 5 });
   textSize(mSize);
   fill(255);
   textAlign(CENTER, CENTER);
-  text(word, width / 2, height / 2, width-300,height);
+  text(word, width / 2, height / 2, width - 300, height);
 }
